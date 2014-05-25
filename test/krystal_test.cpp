647bf059fc8bd40284f16b4048b4f1a4b98eee2f
Arthur Langereis <puurtuur@me.com>
2014-05-25 19:37:21 +0200

    take disk i/o out of the perf test equation

--- test/krystal_test.cpp
+++ test/krystal_test.cpp
@@ -47,6 +47,28 @@ namespace krystal {
 }
 
 
+static std::string readTextFile(const std::string & filePath) {
+	std::ifstream file { filePath };
+	if (! file.is_open()) {
+		return "CANNOT FIND FILE " + filePath;
+	}
+	
+	file.unsetf(std::ios::skipws);
+	file.seekg(0, std::ios::end);
+	auto fileSize = static_cast<size_t>(file.tellg());
+	
+	std::string contents;
+	std::vector<char> buffer(fileSize);
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+	
+	contents.assign(buffer.begin(), buffer.end());
+	return contents;
+}
+
+
+
+
 #include "test_value.hpp"
 #include "test_reader.hpp"
 #include "test_jsonchecker.hpp"
--- test/test_performance.hpp
+++ test/test_performance.hpp
@@ -6,27 +6,27 @@ void test_performance() {
 		using namespace std::chrono;
 		
 		test("medium sized compact file (200KB)", []{
-			std::ifstream perf_file{ "perftests/medium-large.json" };
+			auto perf_file = readTextFile("perftests/medium-large.json");
 			auto t0 = high_resolution_clock::now();
-			auto doc = krystal::parseStream(perf_file);
+			auto doc = krystal::parseString(perf_file);
 			auto t1 = high_resolution_clock::now();
 			
 			std::cout << "Perf: medium file took " << duration_cast<milliseconds>(t1 - t0).count() << "ms.\n";
 		});
 
 		test("rapidjson's insane test file (670KB)", []{
-			std::ifstream perf_file{ "perftests/rapidjson-insane.json" };
+			auto perf_file = readTextFile("perftests/rapidjson-insane.json");
 			auto t0 = high_resolution_clock::now();
-			auto doc = krystal::parseStream(perf_file);
+			auto doc = krystal::parseString(perf_file);
 			auto t1 = high_resolution_clock::now();
 			
 			std::cout << "Perf: rapidjson's file took " << duration_cast<milliseconds>(t1 - t0).count() << "ms.\n";
 		});
 
 		test("very large but very simple file (4.1MB)", []{
-			std::ifstream perf_file{ "perftests/large-but-boring.json" };
+			auto perf_file = readTextFile("perftests/large-but-boring.json");
 			auto t0 = high_resolution_clock::now();
-			auto doc = krystal::parseStream(perf_file);
+			auto doc = krystal::parseString(perf_file);
 			auto t1 = high_resolution_clock::now();
 			
 			std::cout << "Perf: large file took " << duration_cast<milliseconds>(t1 - t0).count() << "ms.\n";
