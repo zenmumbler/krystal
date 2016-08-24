1a94864a852082520558f90cf7169ced1bf6fca8
Arthur Langereis <puurtuur@me.com>
2016-08-24 16:56:28 +0200

    add Oj parser tiny performance test

--- test/test_performance.hpp
+++ test/test_performance.hpp
@@ -1,9 +1,21 @@
 // test_performance.hpp - part of krystal_test
-// (c) 2013 by Arthur Langereis (@zenmumbler)
+// (c) 2013-6 by Arthur Langereis (@zenmumbler)
 
 void test_performance() {
 	group("performance tests", []{
 		using namespace std::chrono;
+		
+		test("100.000 parses of tiny file (255B)", []{
+			// this is from the Oj ruby JSON parser test
+			auto perf_file = readTextFile("perftests/teensy.json");
+			auto t0 = high_resolution_clock::now();
+			for (int x = 0; x < 100000; ++x) {
+				 auto doc = krystal::parseString(perf_file);
+			}
+			auto t1 = high_resolution_clock::now();
+			
+			std::cout << "Perf: 100K times tiny file took " << duration_cast<milliseconds>(t1 - t0).count() << "ms.\n";
+		});
 		
 		test("medium sized compact file (200KB)", []{
 			auto perf_file = readTextFile("perftests/medium-large.json");
